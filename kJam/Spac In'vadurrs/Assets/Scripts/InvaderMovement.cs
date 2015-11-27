22ea6f4ecf69dff397a2d37c947dae61197f7b3c
546576 <JW2-DEERING@STU-CZC3280QD5.campus.ads.uwe.ac.uk>
2015-11-27 13:35:51 +0000

    AI movement code updated
    Added movement patterns; AI will now move 5 spaces, move down one, then
    5 in the opposite direction. Delays between each movement for that
    'space invada' feels

--- kJam/Spac In'vadurrs/Assets/Scripts/InvaderMovement.cs
+++ kJam/Spac In'vadurrs/Assets/Scripts/InvaderMovement.cs
@@ -3,27 +3,87 @@ using System.Collections;
 
 public class InvaderMovement: MonoBehaviour {
 
-	public float speed = 30f;
+	public float speed = 30f; //block movement
+	public float downspeed = 40
+		f; //downwards block translation
+	private int direction = 0; //used as a switch for direction
+	private int right = 0;//used to control distance moved to the right
+	private int left = 0; //used to control distance moved to the left
+
 	// Use this for initialization
 	void Start () {
-		StartCoroutine (ConstantMove ());
+		StartCoroutine (ConstantMove ()); //creates coroutine for delay between enemy movement
 	}
 
 	
-	void MoveLeft()
+	void MoveLeft() //controls left movement. Moves 5 spaces before resetting the right movement counter and swapping the direction switch to 2
 	{
-		transform.position += Vector3.left * speed * Time.deltaTime;
+		if (left < 6) 
+		{
+			transform.position += Vector3.left * speed * Time.deltaTime;
+			left++;
+		}
+
+		if (left == 6)
+		{
+
+			right = 0;
+			direction = 2;
+
+		}
 	}
 	
-	void MoveRight ()
+	void MoveRight () //controls right movement. Moves 5 spaces before resetting the left movement counter and swapping the direction switch to 3
 	{
-		transform.position += Vector3.right * speed * Time.deltaTime;
+		if (right < 6) 
+		{
+			transform.position += Vector3.right * speed * Time.deltaTime;
+			right++;
+		}
+
+		if (right == 6) 
+		{
+
+			left = 0;
+			direction = 3;
+
+		}
 	}
 
-	IEnumerator ConstantMove(){
-		while (true) {
-			MoveLeft ();
+	void MoveDown () //code to move all blocks down a row
+	{
+		transform.position += Vector3.down * downspeed * Time.deltaTime;
+	}
+
+
+	IEnumerator ConstantMove(){ //IEnumerator that is responsible for moving the invaders with a slight delay between steps. Controls the direction switches
+		while (true) 
+		{
+			while (direction == 0) //direction switch for right movement
+			{
+				MoveRight ();
+				yield return new WaitForSeconds (1);
+			}
+
+			while (direction == 1) //direction switch for left movement
+			{
+				MoveLeft ();
+				yield return new WaitForSeconds (1);
+			}
+
+			while (direction == 2) //direction switch for a downward movement, then swaps the blocks to move right
+			{
+				MoveDown ();
+				yield return new WaitForSeconds (1); 
+				direction = 0;
+
+			}
+			while (direction == 3)//direction switch for a downward movement, then swaps blocks to move left
+			{
+			MoveDown ();
 			yield return new WaitForSeconds (1);
+				direction = 1;
+			}
 		}
 	}
 
@@ -31,7 +91,7 @@ public class InvaderMovement: MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-
+		ConstantMove();
 	}
 
 
