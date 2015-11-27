5c9c45993c4d0a280cecd0c4fd73d15f4ccb23ee
546576 <JW2-DEERING@STU-CZC3280QD5.campus.ads.uwe.ac.uk>
2015-11-27 12:42:58 +0000

    script folder and move code
    working on making paced space invader movement code

---
+++ kJam/Spac In'vadurrs/Assets/Scripts/InvaderMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InvaderMovement: MonoBehaviour {
+
+	public float speed = 1.5f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+		/*if (Input.GetKeyDown(KeyCode.RightArrow))
+		{ 
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}*/
+	}
+}
