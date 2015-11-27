b81fff231d46464fe149d53f609929c87c10c66a
m2relevo <m2-relevo@STU-CZC3280QCR.campus.ads.uwe.ac.uk>
2015-11-27 13:45:17 +0000

    Movement and boolets

--- kJam/Spac In'vadurrs/Assets/PlayerMovement.cs
+++ kJam/Spac In'vadurrs/Assets/PlayerMovement.cs
@@ -1,15 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
-
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed;
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+		float x = Input.GetAxisRaw ("Horizontal");
+		float y = Input.GetAxisRaw ("Vertical");
+		Vector2 direction = new Vector2 (x, y).normalized;
+
+		Move (direction);
 	}
+
+	void Move(Vector2 direction)
+		{
+		Vector2 min = Camera.main.ViewportToWorldPoint (new Vector2 (0, 0));
+		Vector2 max = Camera.main.ViewportToWorldPoint (new Vector2 (1, 1));
+
+		max.x = max.x - 0.225f;
+		min.x = min.x + 0.225f;
+
+		Vector2 pos = transform.position;
+
+		pos += direction * speed * Time.deltaTime;
+
+		pos.x = Mathf.Clamp (pos.x, min.x, max.x);
+
+		transform.position = pos;
+		}
 }
