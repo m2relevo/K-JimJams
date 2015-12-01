54bf364966962b68ab4e60f249f2b0aff9f1aead
Popesture <georgepope730@hotmail.com>
2015-12-01 18:07:39 +0000

    menu Complete

--- kJam/Spac In'vadurrs/Assets/Scripts/ufoMove.cs
+++ kJam/Spac In'vadurrs/Assets/Scripts/ufoMove.cs
@@ -3,39 +3,43 @@ using System.Collections;
 
 public class ufoMove : MonoBehaviour {
 
-	Vector3 max = Camera.main.ViewportToWorldPoint (new Vector3 (1, 1, 1));
-	Vector3 min = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, 0));
-	float speed = 0.01f;
-	bool moveLeft = false;
+	Vector2 maxCam;
+	Vector2 minCam;
+	float speed = 3f;
 
 	public GameObject leftBox;
 	public GameObject rightBox;
+	public Vector2 position;
 
 	// Use this for initialization
 	void Start () {
-		leftBox = GameObject.FindWithTag ("leftBox");
-		rightBox = GameObject.FindWithTag ("rightBox");
-
-		leftBox.transform.position = new Vector3(min.x, (max.y/2), 1);
-		rightBox.transform.position = new Vector3(max.x, (max.y/2), 1);
+		position = transform.position;
+		maxCam = Camera.main.ViewportToWorldPoint (new Vector2 (1, 1));
+		minCam = Camera.main.ViewportToWorldPoint (new Vector2 (0, 0));
+		setStuff ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector2 position = transform.position;
 		position = new Vector2 (position.x + speed * Time.deltaTime, position.y);
 		transform.position = position;
+	}
+
+	void setStuff()
+	{
+		leftBox.transform.position = new Vector3(minCam.x, gameObject.transform.position.y, 1);
+		rightBox.transform.position = new Vector3(maxCam.x, gameObject.transform.position.y, 1);
 	}
 
 	void OnTriggerEnter2D(Collider2D col)
 	{
 		if (col.gameObject.tag == "rightBox") 
 		{
-			speed = -0.01f;
+			speed = -3f;
 		}
 		if (col.gameObject.tag == "leftBox") 
 		{
-			speed = 0.01f;
+			speed = 3f;
 		}
 	}
 
